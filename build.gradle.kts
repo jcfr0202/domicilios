a7eff64372483213d6a33cd049ed794a97c8f95a
Juan Flórez <jcfr0202@gmail.com>
2020-05-11 19:23:53 -0500

    adding .gitignore file and build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,75 @@
+import org.gradle.api.tasks.testing.logging.TestExceptionFormat
+import org.gradle.api.tasks.testing.logging.TestLogEvent
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+val kotlinVersion: String by project
+val jacksonVersion: String by project
+val springVersion: String by project
+val springDataVersion: String by project
+val jUnitVersion: String by project
+val mockitoVersion: String by project
+val hamcrestVersion: String by project
+val kotlinLoggingVersion: String by project
+val logbackVersion: String by project
+val slf4jKotlinCoroutinesVersion: String by project
+val cassandraDriverVersion: String by project
+val codaHaleMetricsVersion: String by project
+val testContainersVersion: String by project
+val dockerJavaClientVersion: String by project
+val kotlinCoroutinesVersion: String by project
+val commonsIOVersion: String by project
+val groovyVersion: String by project
+val spockVersion: String by project
+val vertxVersion: String by project
+
+plugins {
+    kotlin("jvm") version "1.3.70"
+}
+
+allprojects {
+    group = "com.test.domicilios"
+    project.version = "1.0"
+
+    apply(plugin = "org.jetbrains.kotlin.jvm")
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions {
+            freeCompilerArgs = listOf("-Xjsr305=strict")
+            jvmTarget = "1.8"
+        }
+    }
+
+    repositories {
+        jcenter()
+        mavenCentral()
+        maven(url = "http://dl.bintray.com/ijabz/maven")
+    }
+
+    tasks.withType<Test> {
+        useJUnitPlatform()
+        testLogging {
+            events = setOf(TestLogEvent.PASSED, TestLogEvent.SKIPPED, TestLogEvent.FAILED)
+            exceptionFormat = TestExceptionFormat.FULL
+            showStandardStreams = true
+        }
+    }
+
+    dependencies {
+        // Kotlin
+        implementation(kotlin(module = "stdlib-jdk8", version = kotlinVersion))
+        implementation(kotlin(module = "reflect", version = kotlinVersion))
+
+        // Jackson
+        implementation("com.fasterxml.jackson.core:jackson-databind:$jacksonVersion")
+        implementation("com.fasterxml.jackson.module:jackson-module-kotlin:$jacksonVersion")
+
+        // Logging
+        implementation("io.github.microutils:kotlin-logging:$kotlinLoggingVersion")
+        implementation("ch.qos.logback:logback-classic:$logbackVersion")
+
+        // Test
+        testImplementation("org.junit.jupiter:junit-jupiter-api:$jUnitVersion")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:$jUnitVersion")
+        testImplementation("org.hamcrest:hamcrest-core:$hamcrestVersion")
+    }
+}
